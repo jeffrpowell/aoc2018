fef3a293441083492c5fee080260b815e4715c48
Jeff Powell <jeffr.powell@outlook.com>
2018-12-05 12:06:11 -0700

    days 1-3

---
+++ src/main/java/com/jeffrpowell/aoc2018/Tests.java
@@ -0,0 +1,15 @@
+package com.jeffrpowell.aoc2018;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tests
+{
+    public static final List<String> DAY3 = new ArrayList<>();
+    
+    static {
+        DAY3.add("#1 @ 1,3: 4x4");
+        DAY3.add("#2 @ 3,1: 4x4");
+        DAY3.add("#3 @ 5,5: 2x2");
+    }
+}
