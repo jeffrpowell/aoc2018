884f107822e6d2477658c5639e08d1259913d291
jeffrpowell <jeffr.powell@outlook.com>
2018-12-10 11:26:14 -0700

    day 10

--- src/main/java/com/jeffrpowell/aoc2018/Launcher.java
+++ src/main/java/com/jeffrpowell/aoc2018/Launcher.java
@@ -3,6 +3,6 @@ package com.jeffrpowell.aoc2018;
 public class Launcher
 {
     public static void main(String[] args) {
-        System.out.println(new Day9().part2(Inputs.DAY9));
+        System.out.println(new Day10().part2(Inputs.DAY10));
     }
 }
--- src/main/java/com/jeffrpowell/aoc2018/Tests.java
+++ src/main/java/com/jeffrpowell/aoc2018/Tests.java
@@ -13,6 +13,7 @@ public class Tests
 	public static final List<String> DAY7 = new ArrayList<>();
 	public static final List<Integer> DAY8 = new ArrayList<>();
 	public static final List<Integer> DAY9 = new ArrayList<>();
+	public static final List<String> DAY10 = new ArrayList<>();
     
     static {
         DAY3.add("#1 @ 1,3: 4x4");
@@ -43,5 +44,36 @@ public class Tests
 		while(m9.find()) {
 			DAY9.add(Integer.parseInt(m9.group(1)));
 		}
+        DAY10.add("position=< 9,  1> velocity=< 0,  2>");
+        DAY10.add("position=< 7,  0> velocity=<-1,  0>");
+        DAY10.add("position=< 3, -2> velocity=<-1,  1>");
+        DAY10.add("position=< 6, 10> velocity=<-2, -1>");
+        DAY10.add("position=< 2, -4> velocity=< 2,  2>");
+        DAY10.add("position=<-6, 10> velocity=< 2, -2>");
+        DAY10.add("position=< 1,  8> velocity=< 1, -1>");
+        DAY10.add("position=< 1,  7> velocity=< 1,  0>");
+        DAY10.add("position=<-3, 11> velocity=< 1, -2>");
+        DAY10.add("position=< 7,  6> velocity=<-1, -1>");
+        DAY10.add("position=<-2,  3> velocity=< 1,  0>");
+        DAY10.add("position=<-4,  3> velocity=< 2,  0>");
+        DAY10.add("position=<10, -3> velocity=<-1,  1>");
+        DAY10.add("position=< 5, 11> velocity=< 1, -2>");
+        DAY10.add("position=< 4,  7> velocity=< 0, -1>");
+        DAY10.add("position=< 8, -2> velocity=< 0,  1>");
+        DAY10.add("position=<15,  0> velocity=<-2,  0>");
+        DAY10.add("position=< 1,  6> velocity=< 1,  0>");
+        DAY10.add("position=< 8,  9> velocity=< 0, -1>");
+        DAY10.add("position=< 3,  3> velocity=<-1,  1>");
+        DAY10.add("position=< 0,  5> velocity=< 0, -1>");
+        DAY10.add("position=<-2,  2> velocity=< 2,  0>");
+        DAY10.add("position=< 5, -2> velocity=< 1,  2>");
+        DAY10.add("position=< 1,  4> velocity=< 2,  1>");
+        DAY10.add("position=<-2,  7> velocity=< 2, -2>");
+        DAY10.add("position=< 3,  6> velocity=<-1, -1>");
+        DAY10.add("position=< 5,  0> velocity=< 1,  0>");
+        DAY10.add("position=<-6,  0> velocity=< 2,  0>");
+        DAY10.add("position=< 5,  9> velocity=< 1, -2>");
+        DAY10.add("position=<14,  7> velocity=<-2,  0>");
+        DAY10.add("position=<-3,  6> velocity=< 2, -1>");
     }
 }
