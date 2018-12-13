253af58d726da80c6bc7fe5fb3f5bb91d1602a2a
jeffrpowell <jeffr.powell@outlook.com>
2018-12-13 12:42:02 -0700

    day 13

--- src/main/java/com/jeffrpowell/aoc2018/Launcher.java
+++ src/main/java/com/jeffrpowell/aoc2018/Launcher.java
@@ -3,6 +3,6 @@ package com.jeffrpowell.aoc2018;
 public class Launcher
 {
     public static void main(String[] args) {
-        System.out.println(new Day11().part1(Inputs.DAY11));
+        System.out.println(new Day13().part2(Inputs.DAY13));
     }
 }
